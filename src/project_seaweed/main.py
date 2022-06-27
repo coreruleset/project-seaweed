dc8faa971a0bc3a8faf8f56f12f1d0ef1bfb57c3
vandanrohatgi <vandanrohatgi@gmail.com>
2022-06-27 12:14:26 +0530

    Add logging,cve,waf params for cve tester

--- src/project_seaweed/main.py
+++ src/project_seaweed/main.py
@@ -2,17 +2,39 @@ import click
 from .extract_payload import extract
 from . import __version__
 from .test_cve import cve_tester
+import logging
 
 
 @click.group()
 @click.version_option(version=__version__)
-def main() -> None:
-    pass
+@click.option(
+    "-l",
+    "--level",
+    type=click.Choice(["debug", "info"], case_sensitive=False),
+    help="Choose output mode",
+    default="warning",
+    show_default=True,
+)
+def main(level) -> None:
+    logging.basicConfig(level=level.upper())
 
 
+@click.option(
+    "--cve_id",
+    required=False,
+    help="CVE IDs (CVE-2022-1211, CVE-2022-1609 ...)",
+    default="",
+)
+@click.option(
+    "--waf_url",
+    required=False,
+    help="URL for alternate WAF test (https://cloudflaredomain.com)",
+    show_default=True,
+    default="crs",
+)
 @click.command()
-def tester() -> None:
-    test = cve_tester()
+def tester(cve_id, waf_url) -> None:
+    test = cve_tester(cve_id=cve_id.split(","), waf_url=waf_url)
     test.generate_raw()
 
 
--- src/project_seaweed/test_cve.py
+++ src/project_seaweed/test_cve.py
@@ -1,20 +1,24 @@
 import sys
-from typing_extensions import Self
 import click
 import docker
 import tempfile
 import os
-
+import traceback
 
 class cve_tester:
-    def __init__(self) -> None:
-        self.waf_image = "owasp/modsecurity-crs:apache"
-        self.web_server_image = "httpd"
+    def __init__(self, cve_id: list = [], waf_url: str = "crs") -> None:
+        if waf_url == "crs":
+            self.waf_image = "owasp/modsecurity-crs:apache"
+            self.web_server_image = "httpd"
+            self.waf_name = "crs-waf"
+            self.web_server_name = "httpd-server"
+            self.waf_url = f"http://{self.waf_name}"
+            self.waf_env = ["PARANOIA=4", f"BACKEND=http://{self.web_server_name}"]
+            self.network_name = "seaweed-network"
+        else:
+            self.waf_url = waf_url
+        self.cve_id = cve_id
         self.nuclei_image = "projectdiscovery/nuclei:latest"
-        self.waf_name = "crs-waf"
-        self.web_server_name = "httpd-server"
-        self.network_name = "crs-network"
-        self.waf_env = ["PARANOIA=4", f"BACKEND=http://{self.web_server_name}"]
         self.client = docker.client.from_env()
         self.temp_dir = tempfile.TemporaryDirectory().name
 
@@ -47,15 +51,27 @@ class cve_tester:
             environment=self.waf_env,
         )
 
+    def get_cves(self) -> str:
+        if self.cve_id != [""]:
+            path = "/root/nuclei-templates/cves/{}/{}.yaml"
+            cves = []
+            for cve in self.cve_id:
+                cve = cve.upper()
+                year = cve.split("-")[1]
+                cves.append(path.format(year, cve))
+            return f"-t {','.join(cves)}"
+        else:
+            return "-t cves -pt http"
+
     def create_nuclei(self) -> None:
         self.printer("Creating nuclei container...")
         self.client.containers.run(
             self.nuclei_image,
             remove=True,
             detach=False,
-            network=self.network_name,
+            network=self.network_name or "",
             volumes={self.temp_dir: {"bind": self.temp_dir, "mode": "rw"}},
-            entrypoint=f"nuclei -u http://{self.waf_name} -t /root/nuclei-templates/cves/2022/CVE-2022-26134.yaml -srd {self.temp_dir}",
+            entrypoint=f"nuclei -u {self.waf_url} {self.get_cves()} -srd {self.temp_dir}",
         )
 
     def change_permission(self) -> None:
@@ -77,11 +93,12 @@ class cve_tester:
     def generate_raw(self) -> None:
         try:
             self.printer(f"Results stored in {self.temp_dir}")
-            self.create_crs()
+            if hasattr(self, "waf_name"):
+                self.create_crs()
             self.create_nuclei()
             self.change_permission()
-        except Exception as e:
-            print(e)
+        except Exception:
+            print(traceback.format_exc())
             sys.exit(1)
         print([x for x in os.walk(self.temp_dir)])
 
@@ -90,15 +107,12 @@ class cve_tester:
         try:
             self.web_server_obj.stop()
         except AttributeError as e:
-            print(e)
             pass
         try:
             self.waf_obj.stop()
         except AttributeError as e:
-            print(e)
             pass
         try:
             self.network.remove()
         except AttributeError as e:
-            print(e)
             pass
